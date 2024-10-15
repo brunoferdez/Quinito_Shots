3cbdecbe323f2b7ff05f018e18a38b3bec5168ea
dam2 <dam2@a223e08.dc.domenicoscarlatti.es>
2024-10-15 17:39:59 +0200

    Inicio Proyecto

---
+++ src/quinito_shots/Quinito_Shots.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package quinito_shots;
+
+/**
+ *
+ * @author dam2
+ */
+public class Quinito_Shots {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
