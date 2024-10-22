ffe0739c7381f7ab7866f6b5391902fe38a55285
dam2 <dam2@a223e08.dc.domenicoscarlatti.es>
2024-10-22 16:45:20 +0200

    Nuevo

--- src/quinito_shots/Quinito_Shots.java
+++ src/quinito_shots/Quinito_Shots.java
@@ -15,7 +15,7 @@ public class Quinito_Shots {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("");
+        System.out.println("Iniciando");
     }
     
 }
